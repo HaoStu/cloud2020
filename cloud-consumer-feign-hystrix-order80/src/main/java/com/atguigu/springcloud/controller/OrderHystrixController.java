cb0932462828d57c4c940f25e363756e708f2cc3
hao <760442816@qq.com>
2022-01-23 22:15:06 +0800

    全局服务降级

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 @RestController
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")  //全局的
 public class OrderHystrixController {
     @Resource
     private PaymentHystrixService paymentHystrixService;
@@ -21,14 +23,21 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
-    })
+//    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+//    })
+    @HystrixCommand
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        int age = 10/0;
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         return result;
     }
     public String paymentInfo_TimeOutHandler(@PathVariable("id") Integer id) {
         return "我是消费者80，对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己";
+    }
+
+    //下面是全局fallback方法
+    public String payment_Global_FallbackMethod() {
+        return "Global异常处理信息，请稍后再试,(┬＿┬)";
     }
 }
