6e035ef389ae01e7bb3300dda1ecc3440b9640b6
hao <760442816@qq.com>
2022-01-23 17:27:36 +0800

    Ribbon负载规则替换

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/Application.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/Application.java
@@ -1,8 +1,13 @@
 package com.atguigu.springcloud;
 
+import com.atguigu.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
+@RibbonClient(name = "cloud-payment-service",configuration = MySelfRule.class)
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
