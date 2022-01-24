38566f15925e4b5a3ede08141fedac5f9cf02311
hao <760442816@qq.com>
2022-01-24 19:46:57 +0800

    GateWay常用的Predicate

--- cloud-gateway-gateway9527/src/test/java/com/example/springcloud/ApplicationTests.java
+++ cloud-gateway-gateway9527/src/test/java/com/example/springcloud/ApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.springcloud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.ZonedDateTime;
+
 @SpringBootTest
 class ApplicationTests {
 
     @Test
     void contextLoads() {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
     }
 
 }
