453bbfd1d501027addbead64aefb11c3af6b0fbd
hao <760442816@qq.com>
2022-01-26 20:47:04 +0800

    Sentinel降级-RT

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,8 +1,12 @@
 package com.atguigu.springcloud.alibaba.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 @RestController
 public class FlowLimitController {
     @GetMapping("/testA")
@@ -13,5 +17,16 @@ public class FlowLimitController {
     @GetMapping("/testB")
     public String testB() {
         return "------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("testD 测试RT");
+        return "------testD";
     }
 }
