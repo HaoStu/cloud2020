7b891ab999fead1438e37c80fcd13521b7596750
hao <760442816@qq.com>
2022-01-26 20:54:00 +0800

    Sentinel降级-异常数

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -31,4 +31,11 @@ public class FlowLimitController {
         int age = 10 / 0;
         return "------testD";
     }
+
+    @GetMapping("/testE")
+    public String testE() {
+        log.info("testE 测试异常数");
+        int age = 10 / 0;
+        return "------testE 测试异常数";
+    }
 }
