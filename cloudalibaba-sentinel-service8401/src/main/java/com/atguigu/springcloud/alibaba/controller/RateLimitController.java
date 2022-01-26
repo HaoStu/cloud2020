ebbce8bb048c6654a7e631b48ac14b05b3efbd13
hao <760442816@qq.com>
2022-01-26 21:51:45 +0800

    SentinelResource配置(下)

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/RateLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/RateLimitController.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud.alibaba.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.atguigu.springcloud.alibaba.myhandler.CustomerBlockHandler;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,5 +24,11 @@ public class RateLimitController {
     @SentinelResource(value = "byUrl")
     public CommonResult byUrl() {
         return new CommonResult(200, "按url限流测试OK", new Payment(2020L, "serial002"));
+    }
+
+    @GetMapping("/rateLimit/customerBlockHandler")
+    @SentinelResource(value = "customerBlockHandler", blockHandlerClass = CustomerBlockHandler.class, blockHandler = "handlerException2")
+    public CommonResult customerBlockHandler() {
+        return new CommonResult(200, "按客戶自定义", new Payment(2020L, "serial003"));
     }
 }
